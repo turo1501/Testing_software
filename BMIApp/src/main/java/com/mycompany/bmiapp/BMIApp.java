1041fc145a96716088b6cdf2081d58ad68fd1cb8
turo151001 <tuant32456@gmail.com>
2024-03-04 08:02:55 +0700

    update

---
+++ BMIApp/src/main/java/com/mycompany/bmiapp/BMIApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bmiapp;
+
+/**
+ *
+ * @author turo
+ */
+public class BMIApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
